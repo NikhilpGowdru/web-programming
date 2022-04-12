efedf24f2323410faf6976de621a7bf9ca3fdc6f
NikhilpGowdru <102573799+NikhilpGowdru@users.noreply.github.com>
2022-04-12 10:46:46 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,56 @@
+/*//slice
+let name="bharath";
+let res=name.slice(1,5);
+console.log(res);*/
+
+/*//substing
+let n="ajay";
+let r=n.substring(0,3);
+console.log(r);*/
+
+
+/*//substring
+let na="ajay";
+let rs=na.substr(0,3);
+console.log(rs);*/
+
+/*//replace
+let txt="please visit to ds lab";
+let replace=txt.replace("ds","web");
+console.log(replace);*/
+
+/*//converting upper and lower case
+let le="ab devilliars";
+let u=le.toUpperCase();
+let v=le.toLowerCase();
+console.log(u);
+console.log(v);*/
+
+/*//concatination
+let s1="rabada";
+let s2="smith";
+console.log("String Concatenation=",s1.concat(" ",s2));*/
+
+/*//trim method
+let text1="   Hello World!  ";
+let text2=text1.trim();
+console.log(text2);*/
+
+/*//padstart and padend
+let text3="5";
+let padded=text3.padEnd(4,"a");
+let pad=text3.padStart(4,"a");
+console.log(padded);
+console.log(pad);*/
+
+/*//charAt to charCodeAt
+let text4="HELLO WORLD";
+let char=text4.charCodeAt(0);
+console.log(char);*/
+
+//let num=5;
+let text5=num.toString();
+let padded=text.padEnd
+
+
+
